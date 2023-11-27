d6092bcc4b6021562c3a9ec73476a57fed5dfca4
Adek <eloelo.gmail.com>
2023-11-27 19:52:10 +0100

    Projekt gotowy do wdrozenia w Heroku.

---
+++ learning_logs/urls.py
@@ -0,0 +1,21 @@
+"""definiuje wzorce adresów URL dla learning_logs."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    #Strona głowna
+    path('', views.index, name='index'),
+    #Wyświetlenie wszystkich tematów.
+    path('topics/', views.topics, name='topics'),
+    #Strona szczegółowa dotycząca pojedynczego tematu.
+    path('topics/(<int:topic_id>)/', views.topic, name='topic'),
+    #strona przeznaczona do dodawania nowego tematu.
+    path('new_topic/', views.new_topic, name='new_topic'),
+    #stron przeznaczona do dodawanie nowego wpisu.
+    path('new_entry/(<int:topic_id>)/', views.new_entry, name='new_entry'),
+    #strona przeznaczona do edycji wpisu.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+]
